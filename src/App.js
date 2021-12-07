49b3e6b17f1857367a2ceea8f2e26320b8c7c067
linjunc <ljc15113180751@163.com>
2021-12-07 22:52:12 +0800

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import './App.css';
 import AfterModal from "./components/AfterModal";
 import routes from "./router";
-import { articleContext } from './models/context'
+import { articleContext, userContext } from './models/context'
 import { Article } from "./models/reducer/article.js";
 import { useEffect, useReducer, useRef } from "react";
 import { INIT_ARTICLE } from "./models/constant.js";
@@ -13,22 +13,31 @@ function App() {
   return element
 }
 
-const AppWrapper = () => { 
+const AppWrapper = () => {
   const initRef = useRef([])
+  const userRef = useRef({})
   const [article, dispatch] = useReducer(Article, [])
   useEffect(() => {
+    // 获取本地的稍后再看数据
     initRef.current = JSON.parse(localStorage.getItem('articleLater')) ?? []
     dispatch({
       type: INIT_ARTICLE,
       articleData: initRef.current
     })
+    if (localStorage.getItem("token")) {
+      userRef.current = JSON.parse(localStorage.getItem("token"))
+      
+    }
   }, [])
   return (
     <Router>
-      <articleContext.Provider value={{ article, dispatch }} >
-        <App />
-        <AfterModal />
-      </articleContext.Provider>
+      <userContext.Provider>
+        <articleContext.Provider value={{ article, dispatch }} >
+          <App />
+          <AfterModal />
+        </articleContext.Provider>
+      </userContext.Provider>
+
       <GlobalStyle />
     </Router>
   )
