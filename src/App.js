2255597dba9f419dc3c69f4bc2c1d38d30236aea
dengchongchang <dsz13413090799@outlook.com>
2021-12-08 21:45:20 +0800

    feat: 年度报告模块页面已搭建完成并增加了下载报告的功能

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { GlobalStyle } from "./style.js";
-import { BrowserRouter as Router, useRoutes } from "react-router-dom";
-import './App.css';
-import AfterModal from "./components/AfterModal";
-import routes from "./router";
+import { GlobalStyle } from './style.js'
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
+import './App.css'
+import AfterModal from './components/AfterModal'
+import routes from './router'
 import { articleContext, userContext } from './models/context'
-import { Article } from "./models/reducer/article.js";
-import { useEffect, useReducer, useRef } from "react";
-import { INIT_ARTICLE } from "./models/constant.js";
+import { Article } from './models/reducer/article.js'
+import { useEffect, useReducer, useRef } from 'react'
+import { INIT_ARTICLE } from './models/constant.js'
 
 function App() {
   const element = useRoutes(routes)
@@ -22,24 +22,23 @@ const AppWrapper = () => {
     initRef.current = JSON.parse(localStorage.getItem('articleLater')) ?? []
     dispatch({
       type: INIT_ARTICLE,
-      articleData: initRef.current
+      articleData: initRef.current,
     })
-    if (localStorage.getItem("token")) {
-      userRef.current = JSON.parse(localStorage.getItem("token"))
-      
+    if (localStorage.getItem('token')) {
+      userRef.current = JSON.parse(localStorage.getItem('token'))
     }
   }, [])
   return (
     <Router>
-      <userContext.Provider>
-        <articleContext.Provider value={{ article, dispatch }} >
-          <App />
-          <AfterModal />
-        </articleContext.Provider>
-      </userContext.Provider>
+      {/* <userContext.Provider> */}
+      <articleContext.Provider value={{ article, dispatch }}>
+        <App />
+        <AfterModal />
+      </articleContext.Provider>
+      {/* </userContext.Provider> */}
 
       <GlobalStyle />
     </Router>
   )
 }
-export default AppWrapper;
+export default AppWrapper
--- src/pages/user/c-cpns/reading-report/index.js
+++ src/pages/user/c-cpns/reading-report/index.js
@@ -1,91 +1,92 @@
-import React, { memo, useState } from 'react'
+import React, { memo, useRef, useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
+import html2canvas from 'html2canvas'
 
-import ReportGraph from './c-cpns/report-graph'
+import { Button } from 'antd'
+import PieChart from './c-cpns/pie-chart'
+import InstrumentChart from './c-cpns/Instrument-chart'
+import BarChart from './c-cpns/bar-chart'
 
-import {
-  ReadingReportWrapper,
-  CategoryRankWrapper,
-  TransitionWrapper
-} from './style'
+import { ReadingReportWrapper, TransitionWrapper } from './style'
 
 export default function ReadingReport() {
   // 用户年度总结阅读时间排行榜
   const [readTimeArr, setReadTimeArr] = useState([
     {
       title: '热点类',
-      readTime: 320
+      readTime: 320,
     },
     {
       title: '财经类',
-      readTime: 300
+      readTime: 300,
     },
     {
       title: '科技类',
-      readTime: 290
+      readTime: 290,
     },
     {
       title: '国际类',
-      readTime: 240
+      readTime: 240,
     },
     {
       title: '体育类',
-      readTime: 230
+      readTime: 230,
     },
     {
       title: '娱乐类',
-      readTime: 180
+      readTime: 180,
     },
     {
       title: '数码类',
-      readTime: 160
+      readTime: 160,
     },
     {
       title: '游戏类',
-      readTime: 150
+      readTime: 150,
     },
     {
       title: '时尚类',
-      readTime: 100
+      readTime: 100,
     },
     {
       title: '美食类',
-      readTime: 60
-    }
+      readTime: 60,
+    },
   ])
 
-  // 获取最多的阅读时间
-  const maxTime = readTimeArr[0].readTime
+  const ref = useRef()
 
-  // 根据最大阅读时间计算每个柱状图长度的函数
-  const calculated = readTime => readTime / maxTime * 650 + 'px'
+  const downLoad = () => {
+    console.log(ref)
+    html2canvas(ref.current).then(function (canvas) {
+      console.log(canvas)
+      // const imgUrl = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream'); // 获取生成的图片的url
+      const imgUrl = canvas.toDataURL('image/png')
+      console.log(imgUrl)
+      // 生成一个a元素
+      const a = document.createElement('a')
+      // 建一个单击事件
+      const event = new MouseEvent('click')
+      // 将a的download属性设置为我们想要下载的图片名称，若name不存在则使用‘下载图片名称’作为默认名称
+      a.download = 'iNews年度报告总结'
+      // 将生成的URL设置为a.href属性
+      a.href = imgUrl
+      // 触发a的单击事件
+      a.dispatchEvent(event)
+      // document.body.appendChild(canvas);
+    })
+  }
 
   return (
     <TransitionWrapper>
-      <CSSTransition in={true}
-        classNames="report"
-        timeout={1000}
-        appear={true}
-      >
-        <ReadingReportWrapper>
+      <Button onClick={downLoad}>下载图片</Button>
+      <CSSTransition in={true} classNames="report" timeout={1000} appear={true}>
+        <ReadingReportWrapper ref={ref}>
           <div className="main-content">
             <div className="title-img"></div>
-            <ReportGraph />
-            {/* <CategoryRankWrapper>
-              {
-                readTimeArr.map((item, index) => {
-                  const { title, readTime } = item
-                  return (
-                    <div key={title}>
-                      <span className="category">{`${index + 1}. ${title}`}</span>
-                      <div className="bar-chart">
-                        <div className="num" style={{ width: calculated(readTime) }}>{`${readTime} min`}</div>
-                      </div>
-                    </div>
-                  )
-                })
-              }
-            </CategoryRankWrapper> */}
+            <PieChart />
+            <InstrumentChart />
+            <BarChart />
           </div>
         </ReadingReportWrapper>
       </CSSTransition>
