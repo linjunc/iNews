c3f1eaa9c5e845f2642b06c3b86f288ddc8aa371
linjunc <ljc15113180751@163.com>
2021-12-19 18:56:26 +0800

    style: add hover color

--- src/pages/detail/components/ArticleSide/style.js
+++ src/pages/detail/components/ArticleSide/style.js
@@ -1,39 +1,42 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const ArticleContainer = styled.aside`
-    .author-article {
-            display: flex;
-            margin: 20px 0;
-            cursor: pointer;
-            .article-list-img {
-                margin-right: 10px;
-                img {
-                    width: 94px;
-                    height: 70px;
-                }
-            }
-            .article-list-right {
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                .article-list-title {
-                    max-height: 48px;
-                    font-size: 16px;
-                    line-height: 24px;
-                    color: #222;
-                    display: -webkit-box;
-                    overflow: hidden;
-                    -webkit-line-clamp: 2;
-                    -webkit-box-orient: vertical;
-                    text-overflow: ellipsis;
-                }
-                .article-list-num {
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: space-between;
-                    font-size: 12px;
-                    color: #999999;
-                }
-            }
-        } 
+  .author-article {
+    display: flex;
+    margin: 20px 0;
+    cursor: pointer;
+    .article-list-img {
+      margin-right: 10px;
+      img {
+        width: 94px;
+        height: 70px;
+      }
+    }
+    .article-list-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .article-list-title {
+        max-height: 48px;
+        font-size: 16px;
+        line-height: 24px;
+        color: #222;
+        display: -webkit-box;
+        overflow: hidden;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        text-overflow: ellipsis;
+        :hover {
+          color: #1890ff;
+        }
+      }
+      .article-list-num {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        font-size: 12px;
+        color: #999999;
+      }
+    }
+  }
 `
